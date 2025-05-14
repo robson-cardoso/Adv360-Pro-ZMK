97a22221f9b74ccdadd0b7019ac8ac7f98ca1023
Robson Cardoso <robson.cardoso@inflow-analytics.com>
2025-05-14 12:57:15 -0300

    refactor: Build legacy firmware locally

--- bin/build.sh
+++ bin/build.sh
@@ -7,9 +7,11 @@ TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M")}"
 COMMIT="${COMMIT:-$(echo xxxxxx)}"
 
 # West Build (left)
-west build -s zmk/app -p -d build/left -b adv360_left -S studio-rpc-usb-uart -- -DZMK_CONFIG="${PWD}/config" -DCONFIG_ZMK_STUDIO=y
+west build -s zmk/app -d build/left -b adv360_left -- -DZMK_CONFIG="${PWD}/config"
+
 # Adv360 Left Kconfig file
 grep -vE '(^#|^$)' build/left/zephyr/.config
+
 # Rename zmk.uf2
 cp build/left/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-left-clique.uf2"
 
